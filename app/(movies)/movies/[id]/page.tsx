128c4e0915bb1579b74e34c63440dbd9907feb4f
mignonwhale <mignonwhale@gmail.com>
2024-05-08 08:39:08 +0900

    유동적인 metadata 사용방법

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,14 +1,25 @@
 import { Suspense } from 'react';
-import MovieInfo from '../../../../components/movie-info';
+import MovieInfo, { getMovie } from '../../../../components/movie-info';
 import MovieVideos from '../../../../components/movie-videos';
-import { Metadata } from 'next';
 
-
-export const metadata: Metadata = {
-  title: 'MovieDetail',
+interface IParams {
+  params: { id: string }
 }
 
-export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
+// 고정방식
+// export const metadata: Metadata = {
+//   title: 'MovieDetail',
+// }
+
+// 유동방식
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id); // 첫 호출이라면 이 때 캐싱된다. 이 후 같은 메서드를 호출하면 캐싱된 값이 반환된다.
+  return {
+    title: movie.title,
+  }
+}
+
+export default async function MovieDetail({ params: { id } }: IParams) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
